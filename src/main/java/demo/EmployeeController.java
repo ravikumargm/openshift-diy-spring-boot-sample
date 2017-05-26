5baa2480465f52339522c17adcc51f62667ad097
ravikumargm <ravikumargmsagar@gmail.com>
2017-05-26 15:53:38 +0530

    added CORS globally

--- src/main/java/demo/EmployeeController.java
+++ src/main/java/demo/EmployeeController.java
@@ -7,7 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
  
 import demo.model.Employee;
- 
+
+@CrossOrigin(maxAge = 3600) 
 @RestController
 public class EmployeeController 
 {
